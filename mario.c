dab0d9534b7e131082544fa2fe181c08cfc8f20f
Buschhen <Buschhen@users.noreply.github.com>
2020-04-06 08:50:40 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -10,8 +10,8 @@ int main(void)
     number = get_int("Height: ");
     //check if number is between 1 and 8
     }while(number < 1 || number > 8);
+    printf("\n");
     for(int i = 0; i < number+1; i++){
-        printf("\n");
 //        int counter = 1
 //        for(int spaceleft = 0; spaceleft > i){
 //            printf(" ")
