2501fb0bf6f215e4e7070c5c8ab9612bcd4f3cc6
buschhen <buschhen@users.noreply.github.com>
2020-04-08 11:33:11 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -1,81 +1,91 @@
+// All includings
 # include <cs50.h>
 # include <stdio.h>
 # include <string.h>
 # include <math.h>
 
+// Telling compiler in code blow are these functions
 int words(string text);
 int letters(string text);
 int sentences(string text);
 
+// Start of main function
 int main(void)
 {
+    // Get string from user
     string text = get_string("TEXT: ");
-    
-    float index,L,S,word = words(text);
+    // Initialize vars for math operation
+    float index, L, S, word = words(text);
+    // how many letters in 100 words and how many sentences per 100 words
     float to100 = 100 / word;
     L = to100 * letters(text);
     S = to100 * sentences(text);
+    // Getting the final number for which grade
     index = 0.0588 * L - 0.296 * S - 15.8;
-    if (index < 1 )
+    if (index < 1)
     {
         printf("Before Grade 1\n");    
-    }else if (index >= 16)
+    }
+    else if (index >= 16)
     {
         printf("Grade 16+\n");
-    }else
+    }
+    else
     {
         printf("Grade %.0f\n", round(index));
     }
     
-    //printf("%i letter (s)\n%i word (s)\n%i sentence (s)\n", letters(text),words(text),sentences(text));
-    //printf("Grade %.0f\n",index);
+
 
 }
 
-
+//function for how many words
 int words(string text)
 {
+    // setting words to one becasue there is always 1 word more than spaces
     int words = 1;
     for (int i = 0,  n = strlen(text); i < n; i++)
     {
-       if(text[i] == 32)
-       {
-           words++;
-       }
+        if (text[i] == 32)
+        {
+            words++;
+        }
     }
     return words;
 }
 
-//It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.
-//When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about his injury. His left arm was somewhat shorter than his right; when he stood or walked, the back of his hand was at right angles to his body, his thumb parallel to his thigh.
- //space ! ' , . ; ?
 
 int letters(string text)
 {
+    //initializing letter
     int letters = 0;
     for (int i = 0,  n = strlen(text); i < n; i++)
     {
-       if (text[i] == 32 ||text[i] == 33 || text[i] == 39 ||text[i] == 44 || text[i] == 45 || text[i] == 46 || text[i] == 59 || text[i] == 63 || text[i] == 96)
-       {
+        //dont count any spezial signs like . or ,
+        if (text[i] == 32 || text[i] == 33 || text[i] == 39 || text[i] == 44 || text[i] == 45 || text[i] == 46 || text[i] == 59
+            || text[i] == 63 || text[i] == 96)
+        {
 
-       }
-       else
-       {
-           letters++;
-       }
+        }
+        else
+        {
+            letters++;
+        }
     }
     return letters;
 }
 
+// for home many sentences in string
 int sentences(string text)
 {
     int sentences = 0;
     for (int i = 0,  n = strlen(text); i < n; i++)
     {
-       if (text[i] == 33 ||text[i] == 46 || text[i] == 63)
-       {
+        //if there is a . or ! or ? the sentence is finished
+        if (text[i] == 33 || text[i] == 46 || text[i] == 63)
+        {
             sentences++;
-       }
+        }
     }
     return sentences;
 }
